94b1569bd713d25c6bbee6b871574ef82fccc661
pwmurphy89 <pwmurphy89@gmail.com>
2016-04-25 10:22:15 -0400

    Express Review
    Init project that will use express and node

---
+++ app.js
@@ -0,0 +1,69 @@
+
+//Step ONE: INit app with the express generator--express electricProject.  This will create entire folder structure
+// with everything we need to start making an express web aplliction
+//STep TWO: Add gitignore file that will atleast ignore everything inside node_modules
+//If someone wants to run your app, they can do an npm install themselves
+//STep THREE: Copy the compass boilerplate into the project
+//This will give you access to compass.  You need to change path of css write directory to ../public/stylesheets
+//Init git the repo
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;

d2ecc3c0ca4921b5fefb00274bdb18523c4c9b13
pwmurphy89 <pwmurphy89@gmail.com>
2016-04-26 15:03:37 -0400

    work with index.js

--- routes/index.js
+++ routes/index.js
@@ -5,49 +5,88 @@ var mongoUrl = process.env.	MONGOLAB_URI ||
 			   process.env.MONGOHQ_URL ||
 			   'mongodb://localhost:27017/electricProject';
 var db;
-var allPhotos;
-//get pictures from database
-MongoClient.connect(mongoUrl, function(error, database) {
-	database.collection('cars').find().toArray(function(error, result){
-		allPhotos = result;
-		db = database;
-		console.log(allPhotos);
-	})
-})
 
-/* GET home page. */
+MongoClient.connect(mongoUrl, function(error, database) {
+	db = database;
+	});
+
 router.get('/', function(req, res, next) {
 	var currIP = req.ip;
 	console.log("The current user's IP address is " + currIP);
 	db.collection('users').find({ip:currIP}).toArray(function(error, userResult){
-		//if user result returns nothing, then user hasn't voted on anything
-		if(userResult.length == 0){
-			photosToShow = allPhotos;
+		var photosVoted = [];
+		for (var i =0; i < userResult.length;i++){
+			photosVoted.push(userResult[i].image);
 		}
-		var randomNum = Math.floor(Math.random() * photosToShow.length);
-		res.render('index', { carImage:allPhotos[randomNum].imageSrc});
-	})
+		db.collection('cars').find({imageSrc: {$nin: photosVoted}}).toArray(function(error, photosToShow){
+			if (photosToShow.length == 0){
+				res.redirect('/standings');
+			}else{
+				var randomNum = Math.floor(Math.random() * photosToShow.length);
+				res.render('index', { carImage:photosToShow[randomNum].imageSrc});
+			}
+		});
+	});
 });
-//Set up post electric page
-router.get('/electric', function(req, res, next){
-	res.send(req.body)
+
+router.get('/standings', function(req, res, next){
+	db.collection('cars').find().toArray(function(error, result){
+		standingsArray = [];
+		for (i=0; i<result.length; i++){
+			standingsArray.push(result[i]);
+		}
+		standingsArray.sort(function(a,b){
+			return (b.totalVotes - a.totalVotes);
+		})
+		res.render('standings', {theStandings: standingsArray});
+	});
 });
 
 router.post('/electric', function(req, res, next){
-	// res.send(req.body)
-	db.collection('cars').updateOne(
-		{ imageSrc: req.body.photo },
-		{
-			$set: {"totalVotes": 1}
-		}, function(error, results){
-			console.log(results);
+	db.collection('users').insertOne({
+		ip: req.ip,
+		vote: 'electric',
+		image: req.body.photo
+	});
+	db.collection('cars').find({imageSrc: req.body.photo}).toArray(function(error, result){
+		if(isNaN(result[0].totalVotes)){
+			total = 0;
+		}else{
+			total = result[0].totalVotes;
 		}
-	)
+		db.collection('cars').updateOne(
+			{ imageSrc: req.body.photo },
+			{ $set: {"totalVotes": (total + 1)}},
+			function(error, results){
+				console.log(db.collection('cars'));
+			}
+		);
+	});
+	res.redirect('/');
+});
 
-	res.send("The user chose " + req.body.photo + " as an electric picture");
-});
 router.post('/poser', function(req, res, next){
-	// res.send(req.body)
-	res.send("The user chose " + req.body.photo + " as a poser picture");
+	db.collection('users').insertOne({
+		ip: req.ip,
+		vote: 'poser',
+		image: req.body.photo
+	});  
+  	db.collection('cars').find({imageSrc:req.body.photo}).toArray(function(error, result){
+  		if(isNaN(result[0].totalVotes)){
+  			total = 0;
+  		}else{
+  			total = result[0].totalVotes;
+  		}
+		db.collection('cars').updateOne(
+			{ imageSrc: req.body.photo },
+			{
+				$set: {"totalVotes": (total - 1)}
+			}, function(error, results){
+				// console.log(results);
+			}
+		);
+  	});  
+	res.redirect('/');
 });
+
 module.exports = router;
